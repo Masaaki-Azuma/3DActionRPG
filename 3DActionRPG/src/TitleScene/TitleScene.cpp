6a0c93d00a3c072e00756164355a6bfd6ff68ae3
Azuma <g021g1256@g.neec.ac.jp>
2022-02-14 00:33:08 +0900

    タイトルシーンを作成し、マップの生成処理を移動した

---
+++ 3DActionRPG/src/TitleScene/TitleScene.cpp
@@ -0,0 +1,44 @@
+#include "TitleScene.h"
+#include <DxLib.h>
+#include "Input.h"
+#include "MapScene/MapManager.h"
+
+void TitleScene::start(void* data)
+{
+    is_end_ = false;
+
+}
+
+void TitleScene::update(float delta_time)
+{
+    //ForDebug:シーンを抜け出す仮動作
+    if (Input::get_button_down(PAD_INPUT_1))
+    {
+        is_end_ = true;
+    }
+}
+
+void TitleScene::draw() const
+{
+    DxLib::DrawString(0, 0, "タイトルシーン", GetColor(255, 255, 255));
+}
+
+bool TitleScene::is_end() const
+{
+    return is_end_;
+}
+
+std::string TitleScene::next() const
+{
+    return "MapScene";
+}
+
+void TitleScene::end()
+{
+    MapManager::GetInstance().generate();
+}
+
+void* TitleScene::data()
+{
+    return nullptr;
+}
