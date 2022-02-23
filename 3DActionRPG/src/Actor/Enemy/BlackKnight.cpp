fc5835b4f669ccb5f57c8ea97b340b32201b1e10
Azuma <g021g1256@g.neec.ac.jp>
2022-02-23 23:04:44 +0900

    blackknightを仮実装

--- 3DActionRPG/src/Actor/Enemy/BlackKnight.cpp
+++ 3DActionRPG/src/Actor/Enemy/BlackKnight.cpp
@@ -2,38 +2,212 @@
 
 #include <cassert>
 
-#include "AssetsManager/Mesh.h"
-#include "AssetsManager/PlayerDatabase.h"
 #include "Util/DxConverter.h"
 #include "Util/MyMath.h"
+#include "AssetsManager/Mesh.h"
+#include "AssetsManager/PlayerDatabase.h"
 #include "BattleScene/IWorld.h"
 
-const float DetectionRadius{ 400.0f }; //プレイヤーを検知する範囲半径
-const float AttackRadius{ 50.0f };
+enum
+{
+	Motion_Attack01 = 0,
+	Motion_Attack02 = 1,
+	Motion_Attack03 = 2,
+	Motion_Die = 3,
+	Motion_Dizzy = 4,
+	Motion_Damage = 5,
+	Motion_Idle = 6,
+	Motion_Defence = 7,
+	Motion_Guard = 8,
+	Motion_Run = 10,
+	Motion_WalkBackward = 15,
+	Motion_WalkForward = 16
+};
+
+
+const float DetectionRadius{ 800.0f }; //プレイヤーを検知する範囲半径
+const float AttackRadius{ 350.0f };  //プレイヤーを攻撃する範囲半径
+const float EscapeRadius{ 200.0f };
 const float MoveSpeed{ 200.0f };
 
 BlackKnight::BlackKnight(IWorld* world, const Vector3& position, const Vector3& rotation)
 {
 	assert(DetectionRadius >= AttackRadius && "プレイヤー感知半径が不正です");
 
-	mesh_ = SkinningMesh{ Mesh::black_knight_handle, 20.0f };
 	world_ = world;
 	name_ = "BlackKnight";
 	tag_ = "EnemyTag";
 	position_ = position;
 	rotation_ = rotation;
-	collider_ = Sphere{ 50.0f, Vector3{0.0f, 20.0f, 0.0f} };
-	motion_ = 0;
-	//parameter_ = Parameter{ 500, 100 };
+	collider_ = Sphere{ 200.0f, Vector3{0.0f, 20.0f, 0.0f} };
+	mesh_ = SkinningMesh{ Mesh::black_knight_handle, 20.0f };
+	motion_ = Motion_Idle;
+	parameter_ = e_DB_.get_parameter(name_);
 
 	//メッシュ姿勢初期化
 	mesh_.change_anim(motion_, motion_loop_, motion_interruption);
 	mesh_.set_position(position_);
 	mesh_.set_rotation(rotation_ * MyMath::Deg2Rad);
-	mesh_.set_scale(Vector3{ 2.0f, 2.0f, 2.0f });
+	mesh_.set_scale(Vector3{ 1.0f, 1.0f, 1.0f });
+}
 
+void BlackKnight::react(Actor& other)
+{
+	if (other.tag() == "PlayerAttackTag") {
+		//プレイヤーの攻撃力分ダメージを受ける
+		take_damage(PlayerDatabase::GetInstance().get_current_parameter().attack);
+		if (parameter_.hp <= 0) {
+			//当たり判定を無効化
+			enable_collider_ = false;
+			//死亡状態に遷移
+			change_state(State::Dead, Motion_Die, false);
+			mesh_.change_anim(motion_, motion_loop_, motion_interruption);
+			return;
+		}
+		//ダメージ状態に
+		change_state(State::Damage, Motion_Damage, false, true);
+		mesh_.change_anim(motion_, motion_loop_, motion_interruption);
+	}
 }
 
 void BlackKnight::update_state(float delta_time)
 {
+	switch (state_) {
+	case State::Move: move(delta_time); break;
+	case State::Attack: attack(delta_time); break;
+	case StateBK::Tackle: tackle(delta_time); break;
+	case StateBK::Slash: slash(delta_time); break;
+	case State::Damage: damage(delta_time); break;
+	case State::Dead: dead(delta_time); break;
+	}
+
+	state_timer_ += delta_time;
+}
+
+void BlackKnight::move(float delta_time)
+{
+	//プレイヤーが存在しなかったら棒立ち状態
+	Actor* player = world_->find_actor("Player");
+	if (!player) {
+		change_state(State::Move, Motion_Idle);
+		return;
+	}
+
+	unsigned int motion = Motion_Idle;
+	Vector3 velocity = Vector3::ZERO;
+
+	//プレイヤー方向
+	Vector3 direction = player->position() - position_;
+	direction.y = 0.0f;
+	//プレイヤーとの距離
+	float distance = direction.Length();
+	
+	//距離により状態
+	if (distance <= EscapeRadius) {
+		//プレイヤーから離れる移動量
+		velocity = direction.Normalize() * -MoveSpeed;
+		//向きを求める
+		rotation_.y = Vector3::SignedAngleY(Vector3::FORWARD, direction) * MyMath::Rad2Deg;
+		//後ろ歩きモーションを設定
+		motion = Motion_WalkBackward;
+
+	}
+	else if (distance <= AttackRadius) {
+		//攻撃判定を取得
+		Vector3 pos_attack = position_ + forward() * 100.0f;
+		//generate_attack(Sphere{ 50.0f, pos_attack }, name_ + "Attack", 0.5f, 0.4f);
+		//攻撃状態に遷移
+		change_state(State::Attack, Motion_Attack02, false);
+		return;
+	}
+	else if (distance <= DetectionRadius) {  //移動状態
+		//プレイヤーに近づく移動量
+		velocity = direction.Normalize() * MoveSpeed;
+		//向きを求める
+		rotation_.y = Vector3::SignedAngleY(Vector3::FORWARD, direction) * MyMath::Rad2Deg;
+		//前歩きモーションを設定
+		motion = Motion_WalkForward;
+
+		//一定時間移動状態が続いたらタックル
+		if (state_timer_ >= 3.0f) {
+			change_state(StateBK::Tackle, Motion_Attack03, false);
+			return;
+		}
+	}
+
+	velocity_ = velocity;
+	position_ += velocity_ * delta_time;
+	change_motion(motion);
+	//change_state(State::Move, motion);
+}
+
+void BlackKnight::attack(float delta_time)
+{
+	if (state_timer_ >= 1.0f && !has_attacked) {
+		has_attacked = true;
+		Vector3 pos_attack = position_ + forward() * 300.0f;
+		generate_attack(Sphere{ 150.0f, pos_attack }, name_ + "Attack", 0.5f);
+
+	}
+	if (state_timer_ >= mesh_.anim_total_sec()) {
+		has_attacked = false;
+		change_state(State::Move, Motion_Idle);
+	}
+}
+
+void BlackKnight::slash(float delta_time)
+{
+	if (state_timer_ >= 0.6f && !has_attacked) {
+		has_attacked = true;
+		Vector3 pos_attack = position_ + forward() * 300.0f;
+		generate_attack(Sphere{ 150.0f, pos_attack }, name_ + "Attack", 0.2f);
+
+	}
+	if (state_timer_ >= mesh_.anim_total_sec()) {
+		has_attacked = false;
+		change_state(State::Move, Motion_Idle);
+	}
+}
+
+void BlackKnight::tackle(float delta_time)
+{
+	if (state_timer_ >= 0.6f && !has_attacked) {
+		has_attacked = true;
+		Vector3 pos_attack = position_ + forward() * 500.0f;
+		generate_attack(Sphere{ 250.0f, pos_attack }, name_ + "Attack", 0.3f);
+	}
+
+	if (state_timer_ >= mesh_.anim_total_sec()) {
+		has_attacked = false;
+		change_state(State::Move, Motion_Idle);
+	}
+}
+
+void BlackKnight::damage(float delta_time)
+{
+	
+	if (state_timer_ >= mesh_.anim_total_sec()) {
+		change_state(StateBK::Slash, Motion_Attack01, false);
+	}
+}
+
+void BlackKnight::dead(float delta_time)
+{
+	if (state_timer_ >= mesh_.anim_total_sec()) {
+		//敵討伐数を加算
+		world_->add_basterd(name_);
+		die();
+	}
+}
+
+void BlackKnight::draw_debug() const
+{
+	//ForDebug:
+	static const int blue = DxLib::GetColor(0, 0, 255);
+	static const int yellow = DxLib::GetColor(255, 255, 0);
+	static const int red = DxLib::GetColor(255, 0, 0);
+	DrawSphere3D(DxConverter::GetVECTOR(position_), EscapeRadius, 4, blue, blue, false);
+	DrawSphere3D(DxConverter::GetVECTOR(position_), DetectionRadius, 4, yellow, yellow, false);
+	DrawSphere3D(DxConverter::GetVECTOR(position_), AttackRadius, 4, red, red, false);
+	DxLib::DrawFormatString(0, 20, DxLib::GetColor(255, 255, 255), "blackKnight_hp:%d", parameter_.hp);
 }
