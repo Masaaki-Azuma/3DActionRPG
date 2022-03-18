ec75842fb174ca0d699ef1d4d160ae55f204d648
Azuma <g021g1256@g.neec.ac.jp>
2022-03-18 21:06:50 +0900

    パラメーター調整

--- 3DActionRPG/src/BattleScene/EnemySpawner.cpp
+++ 3DActionRPG/src/BattleScene/EnemySpawner.cpp
@@ -21,7 +21,6 @@ void EnemySpawner::spawn(const std::string& enemy)
 		world_.add_actor(std::make_shared<Slime>(&world_, Vector3{ 500.0f, 0.0f, 500.0f }, Vector3{ 0.0f, 180.0f, 0.0f }));
 	}
 	else if (enemy == "Skeleton") {
-		//world_.add_actor(new Skeleton{ &world_,  Vector3{ 0.0f, 0.0f, 500.0f }, Vector3{ 0.0f, 180.0f, 0.0f } });
 		world_.add_actor(std::make_shared<Skeleton>(&world_, Vector3{ -500.0f, 0.0f, 500.0f }, Vector3{ 0.0f, 180.0f, 0.0f }));
 		world_.add_actor(std::make_shared<Skeleton>(&world_, Vector3{ 500.0f, 0.0f, 500.0f }, Vector3{ 0.0f, 180.0f, 0.0f }));
 	}
@@ -35,6 +34,8 @@ void EnemySpawner::spawn(const std::string& enemy)
 	}
 	else if (enemy == "BlackKnight") {
 		world_.add_actor(std::make_shared<BlackKnight>(&world_, Vector3{ 0.0f, 0.0f, 500.0f }, Vector3{ 0.0f, 180.0f, 0.0f }));
+		world_.add_actor(std::make_shared<Slime>(&world_, Vector3{ -500.0f, 0.0f, 500.0f }, Vector3{ 0.0f, 180.0f, 0.0f }));
+		world_.add_actor(std::make_shared<Slime>(&world_, Vector3{ 500.0f, 0.0f, 500.0f }, Vector3{ 0.0f, 180.0f, 0.0f }));
 	}
 	else if (enemy == "None") {
 		/*処理の都合上、スタート地点を表すNoneが渡されることはないので、バグである*/
